771aebe3c8fb10163ca9cdce6f0099737d3b74ca
rssabbir <sabbirhossainrahad@gmail.com>
2022-10-24 23:29:00 +0600

    added /courses path, for get data from frontend

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+const courses = require('./data/courses.json') 
+    
 app.get('/', (req, res) => {
     res.send('learnVerse Server is running')
 })
 
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.listen(port => {
     console.log(`learnVerse server is running ${port}`);

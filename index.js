01409c50123688450a6e116c64189c25ac062ddf
rssabbir <sabbirhossainrahad@gmail.com>
2022-10-24 22:08:40 +0600

    create server for learnVerse

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('learnVerse Server is running')
+})
+
+app.listen(port => {
+    console.log(`learnVerse server is running ${port}`);
+})

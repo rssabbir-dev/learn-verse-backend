cdeb848ff2c479260970784e56d41bd3580bc769
rssabbir <sabbirhossainrahad@gmail.com>
2022-10-25 03:11:54 +0600

    added /category path to get all categories

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+
 app.use(cors());
 
-const courses = require('./data/courses.json') 
-    
+const courses = require('./data/courses.json');
+const category = require('./data/category.json')
+
 app.get('/', (req, res) => {
-    res.send('learnVerse Server is running')
-})
+	res.send('learnVerse Server is running');
+});
 
 app.get('/courses', (req, res) => {
-    res.send(courses)
+	res.send(courses);
+});
+app.get('/category', (req, res) => {
+    res.send(category)
 })
 
-app.listen(port => {
-    console.log(`learnVerse server is running ${port}`);
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
 })
